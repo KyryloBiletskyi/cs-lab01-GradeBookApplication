f3318f1b9b256d70b8fee9f5d0cf0f10b674ba21
Kyrylo Biletskyi <Kyrylo.Biletskyi@microsoft.wsei.edu.pl>
2022-03-06 14:28:54 +0100

    Classes

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,7 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public new Type;
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
---
+++ GradeBook/GradeBooks/StandartGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandartGradeBook : BaseGradeBook
+    {
+        public void StandartGradeBook()
+        {
+            string name;
+        }
+
+
+    }
+}
