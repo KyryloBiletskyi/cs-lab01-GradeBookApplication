f9bb4708f48feb722c723253239de14aadb429de
KyryloBiletskyi <kyrylo.biletskyi@microsoft.wsei.edu.pl>
2022-04-03 20:04:19 +0200

    overrideCalculateStatistics.True

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,7 +34,7 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if (Students.Count > 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
